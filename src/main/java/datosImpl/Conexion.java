8582ea130315903f8fefbdda3e1f8c9ed5a86ecb
David Resca <davidresca9@gmail.com>
2026-05-13 17:53:25 -0300

    UPD Class Conexion. Solo se centraliza la conexion con la db, la responsabilidad de abrir/cerrar conexiones sera gestionada desde los DAOS

--- src/main/java/datosImpl/Conexion.java
+++ src/main/java/datosImpl/Conexion.java
@@ -2,87 +2,16 @@ package datosImpl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Conexion {
 
-	private String host = "jdbc:mysql://localhost:3306/";
-	private String user = "root";
-	private String pass = "root";
-	private String dbName = "sistema_clientes";
+    private String host = "jdbc:mysql://localhost:3306/";
+    private String user = "root";
+    private String pass = "root";
+    private String dbName = "sistema_clientes";
 
-protected Connection connection;
-	
-	public Connection Open()
-	{
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			this.connection = DriverManager.getConnection(host+dbName, user, pass);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		return this.connection;
-	}
-	
-	public ResultSet query(String query, Object... params)
-	{
-		PreparedStatement pst;
-		ResultSet rs = null;
-		try
-		{
-			pst = connection.prepareStatement(query);
-			
-			for (int i = 0; i < params.length; i++) {
-				pst.setObject(i + 1, params[i]);
-			}
-			
-			rs = pst.executeQuery();
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return rs;
-	}
-	
-	public boolean execute(String query, Object... params)
-	{
-		PreparedStatement pst;
-		boolean save = true;
-		try {
-			pst = connection.prepareStatement(query);
-			
-			for (int i = 0; i < params.length; i++) {
-				pst.setObject(i + 1, params[i]);
-			}
-			
-		    pst.executeUpdate();
-		}
-		catch(SQLException e)
-		{
-			save = false;
-			e.printStackTrace();
-		}
-		return save;
-	}
-	
-	public boolean close()
-	{
-		boolean ok=true;
-		try {
-			connection.close();
-		}
-		catch(Exception e)
-		{
-			ok= false;
-			e.printStackTrace();
-		}
-		return ok;
-	}
-	
+    public Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(host + dbName, user, pass);
+    }
 }
