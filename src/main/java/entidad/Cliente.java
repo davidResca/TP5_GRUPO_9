0e7d80c6c11fc6e1cdc9644016748a65f6beb93d
Jonatan Chavez <jonichico400@gmail.com>
2026-05-13 21:31:48 -0300

    Agregue la clase Cliente

---
+++ src/main/java/entidad/Cliente.java
@@ -0,0 +1,155 @@
+package entidad;
+
+public class Cliente {
+	
+	private int id_cliente;
+	private String dni;
+	private String cuil;
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private String sexo;
+	private String fecha_nacimiento;
+	private String direccion;
+	private String nacionalidad;
+	private String localidad;
+	private String provincia;
+	private String correo_electronico;
+	
+	public Cliente(){
+		
+	}
+
+	public Cliente(int id_cliente, String dni, String cuil, String nombre, String apellido, String telefono,
+			String sexo, String fecha_nacimiento, String direccion, String nacionalidad, String localidad,
+			String provincia, String correo_electronico) {
+		this.id_cliente = id_cliente;
+		this.dni = dni;
+		this.cuil = cuil;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+		this.sexo = sexo;
+		this.fecha_nacimiento = fecha_nacimiento;
+		this.direccion = direccion;
+		this.nacionalidad = nacionalidad;
+		this.localidad = localidad;
+		this.provincia = provincia;
+		this.correo_electronico = correo_electronico;
+	}
+
+	public int getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getCuil() {
+		return cuil;
+	}
+
+	public void setCuil(String cuil) {
+		this.cuil = cuil;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(String fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getCorreo_electronico() {
+		return correo_electronico;
+	}
+
+	public void setCorreo_electronico(String correo_electronico) {
+		this.correo_electronico = correo_electronico;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id_cliente=" + id_cliente + ", dni=" + dni + ", cuil=" + cuil + ", nombre=" + nombre
+				+ ", apellido=" + apellido + ", telefono=" + telefono + ", sexo=" + sexo + ", fecha_nacimiento="
+				+ fecha_nacimiento + ", direccion=" + direccion + ", nacionalidad=" + nacionalidad + ", localidad="
+				+ localidad + ", provincia=" + provincia + ", correo_electronico=" + correo_electronico + "]";
+	}
+	
+	
+
+}
